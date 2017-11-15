14742e2bc827dce1b62c92cac7f9c5d0112a6764
narr <nardgu@gmail.com>
2017-11-15 18:56:25 +0800

    add Search and Filter to App container

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -4,5 +4,27 @@ import angular from 'angular';
 
 angular.module('phoneListApp')
   .component('phoneListApp', {
+    controller: PhoneListAppController,
+    controllerAs: 'phoneListAppVm',
     templateUrl: 'app.component.html',
   });
+
+
+PhoneListAppController.$inject = [
+  '$element',
+];
+
+function PhoneListAppController($element) {
+
+  const vm = this;
+
+  vm.$onInit = init;
+  vm.$postLink = postLink;
+
+  function init() {
+    vm.phoneListOrderBy = 'age';
+  }
+
+  function postLink() {}
+
+}
